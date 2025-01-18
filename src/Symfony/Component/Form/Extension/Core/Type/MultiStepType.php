865288b2c1141e3d0f7d96ba9ce385ef39bf8327
Silas Joisten <silasjoisten@proton.me>
2025-01-18 14:06:59 +0200

    Fix Psalm?

--- src/Symfony/Component/Form/Extension/Core/Type/MultiStepType.php
+++ src/Symfony/Component/Form/Extension/Core/Type/MultiStepType.php
@@ -30,11 +30,12 @@ final class MultiStepType extends AbstractType
         $resolver
             ->setRequired('steps')
             ->setDefault('current_step', static function (Options $options): string {
-                if (!\is_string($first = array_key_first($options['steps']))) {
+                $firstStep = array_key_first($options['steps']);
+                if (!\is_string($firstStep)) {
                     throw new \InvalidArgumentException('The option "steps" must be an associative array.');
                 }
 
-                return $first;
+                return $firstStep;
             });
     }
 
